d0192a469bc87018f2c8f2a9db3631d1cb33e040
Furqan <furqanahmedmj@gmail.com>
2023-01-21 02:45:23 +0530

    home

--- project/src/App.js
+++ project/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar } from './Components/Navbar';
 import Slideshow from './Components/Slideshow';
 import Footer from './Components/Footer';
+import Home from './Components/Home';
 import './App.css';
 
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Navbar />
       <Slideshow/>
+      <Home/>
       <Footer/>
     </div>
   );
