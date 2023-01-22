64ece1cf22dacb4295216006e8d1e9b0c3e2ee40
Furqan <furqanahmedmj@gmail.com>
2023-01-22 23:49:39 +0530

    project

--- project/src/Components/Navbar.jsx
+++ project/src/Components/Navbar.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-
+import React from "react";
 
 export function Navbar() {
   return (
     <div id="navbar">
-      <ul id='ul'>
-        <div id='logo'>
-        <img src="https://document-export.canva.com/Usg-Q/DAFX7WUsg-Q/6/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20230118%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230118T130556Z&X-Amz-Expires=86405&X-Amz-Signature=cf097b267ed1914d4f893cefbfa06209045a4327f4a713d238e1abbdfd8f35d4&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2019%20Jan%202023%2013%3A06%3A01%20GMT" alt="" />
-        </div>
-        <a href="/product"><div>Products</div></a>
+      <ul id="ul">
+        <a href="/">
+          <div id="logo">
+            <img
+              src="https://document-export.canva.com/Usg-Q/DAFX7WUsg-Q/6/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20230118%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230118T130556Z&X-Amz-Expires=86405&X-Amz-Signature=cf097b267ed1914d4f893cefbfa06209045a4327f4a713d238e1abbdfd8f35d4&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2019%20Jan%202023%2013%3A06%3A01%20GMT"
+              alt=""
+            />
+          </div>
+        </a>
+        <a href="/product">
+          <div>Products</div>
+        </a>
         <div>Weekly Ads</div>
         <div>Careers</div>
-        <div><input type="text" placeholder="Search Products"/></div>
-        <a href="/delivery">Cart</a>
-        <div>Sign In</div>
+        <a href="/cart">Cart</a>
+        <a href="/orders">Orders</a>
+        <a href="/signup">
+          <div>Sign up</div>
+        </a>
+        <a href="/login">
+          <div>Login</div>
+        </a>
       </ul>
     </div>
   );
--- project/src/Route/AllRoutes.jsx
+++ project/src/Route/AllRoutes.jsx
@@ -1,9 +1,9 @@
 import Home from "../Components/Home";
 import Cart from "../Components/Cart";
 import { Navbar } from "../Components/Navbar";
-import Slideshow from "../Components/Slideshow"
+import Slideshow from "../Components/Slideshow";
 import Footer from "../Components/Footer";
-import Delivery from "../Components/Delivery";
+import Orders from "../Components/Orders";
 import Product from "../Components/Product";
 import { Routes, Route } from "react-router-dom";
 
@@ -16,7 +16,7 @@ function AllRoutes() {
         <Route path="/navbar" element={<Navbar />}></Route>
         <Route path="/slideshow" element={<Slideshow />}></Route>
         <Route path="/footer" element={<Footer />}></Route>
-        <Route path="/delivery" element={<Delivery />}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
         <Route path="/product" element={<Product />}></Route>
       </Routes>
     </>
